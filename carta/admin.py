cc318142f4139911ac66b604f7b04ea9bda96309
pdrj09 <pedro.yt.99@gmail.com>
2025-02-06 10:18:07 +0100

    Feat: admin page update

--- carta/admin.py
+++ carta/admin.py
@@ -2,7 +2,24 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
+from django.contrib import messages
 
 from .models import carta
 
-admin.site.register(carta)
+admin.site.site_header = 'DaCarta'
+admin.site.index_title = 'DaCarta'
+admin.site.site_title = 'DaCarta'
+
+
+class CartaAdmin(admin.ModelAdmin):
+    list_display = ("asunto", "fecha", "etiqueta")
+    search_fields = ("asunto", "etiqueta")
+
+    def MarcarTramitado(modeladmin, request, queryset):
+        queryset.update(etiqueta="Tra")
+        messages.success(request, "Se han marcado las cartas")
+
+    admin.site.add_action(MarcarTramitado, "Marcar como tramitado")
+
+
+admin.site.register(carta, CartaAdmin)
--- carta/models.py
+++ carta/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
+eriquetas = {
+    "Nl" : "No leido",
+    "Tra" : "Tramitado",
+    "NaT" : "No tramitable",
+    "DA" : "General",
+    "IgS" : "Igualdad",
+    "AaE" : "Estudiantes",
+    "Ca" : "Calidad",
+    "Com" : "Comunicacion"
+}
+
 # Create your models here.
 class carta(models.Model):
     asunto = models.CharField(max_length=100)
     texto = models.TextField(max_length=5000)
+    etiqueta = models.CharField(max_length=100, choices=eriquetas, default="Nl")
+    fecha = models.DateField(auto_now_add=True)
+
+
